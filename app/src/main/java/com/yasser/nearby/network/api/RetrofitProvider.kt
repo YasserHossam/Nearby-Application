a866dabab4ba7d95737641b0f1f561bf7e8291ab
Yasser Hossam <yhossam@inmobly.com>
2019-12-27 16:16:18 +0200

    [Network] Add Retrofit provider.

---
+++ app/src/main/java/com/yasser/nearby/network/api/RetrofitProvider.kt
@@ -0,0 +1,60 @@
+package com.yasser.nearby.network.api
+
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+class RetrofitProvider private constructor(){
+    companion object {
+        private lateinit var INSTANCE: Retrofit
+
+        fun getRetrofitInstance(baseUrl: String, clientId: String, clientSecret: String)
+                : Retrofit {
+            if(this::INSTANCE.isInitialized)
+                return INSTANCE
+            else {
+                val httpLoggingInterceptor = HttpLoggingInterceptor()
+                val userlessAuthenticationInterceptor = UserlessAuthenticationInterceptor(clientId,
+                    clientSecret)
+
+                val okHttpClient = OkHttpClient.Builder()
+                    .addInterceptor(userlessAuthenticationInterceptor)
+                    .addInterceptor(httpLoggingInterceptor)
+                    .build()
+
+                val retrofitBuilder = Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .client(okHttpClient)
+                return retrofitBuilder.build()
+            }
+        }
+
+    }
+
+    private class UserlessAuthenticationInterceptor(val clientId: String,
+                                                    val clientSecret: String): Interceptor{
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val original = chain.request()
+            val originalHttpUrl = original.url()
+
+            val url = originalHttpUrl.newBuilder()
+                .addQueryParameter("client_id", clientId)
+                .addQueryParameter("client_secret",clientSecret)
+                .build()
+
+            val requestBuilder = original.newBuilder()
+                .url(url)
+
+            val request = requestBuilder.build()
+            return chain.proceed(request)
+        }
+
+    }
+}
